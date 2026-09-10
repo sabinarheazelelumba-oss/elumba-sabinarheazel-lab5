8573be22db66e39b86db21dfb2523b05c693e7a5
sabinarheazelelumba-oss <sabinarheazelelumba-oss@users.noreply.github.com>
2026-09-10 23:05:20 +0800

    Open product CRUD from application root

--- app/config/routes.php
+++ app/config/routes.php
@@ -44,7 +44,7 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 */
 /** @var object $router **/
 
-$router->get('/', 'Welcome::index');
+$router->get('/', 'AuthController::login');
 $router->get('/users', 'UsersController::index');
 
 $router->get('/login', 'AuthController::login');
